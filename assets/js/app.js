f2377d45ca87f99a1534f73902f1fcb50d62abbd
neginfinity85 <neginfinity85@gmail.com>
2023-08-10 00:58:06 +0200

    Added <ScrollSpy> function

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $(function () {
 	let header = $('#header');
 	let introH = intro.innerHeight();
 	let headerH = header.innerHeight();
+	let scrollTop = $(window).scrollTop();
 
 	/* Header class on scroll
 	=========================================== */
@@ -42,4 +43,37 @@ $(function () {
 			500,
 		);
 	});
+
+	/* ScrollSpy for sections
+	=========================================== */
+	let windowH = $(window).height();
+
+	scrollSpy(scrollTop);
+
+	$(window).on('scroll', function () {
+		scrollTop = $(this).scrollTop();
+
+		scrollSpy(scrollTop);
+	});
+
+	function scrollSpy(scrollTop) {
+		$('[data-scrollspy]').each(function () {
+			let $this = $(this);
+
+			let sectionId = $this.data('scrollspy');
+			let sectionOffset = $this.offset().top;
+
+			sectionOffset -= windowH / 3;
+
+			if (scrollTop >= sectionOffset) {
+				$('[data-scroll]').removeClass('active');
+
+				$('[data-scroll="' + sectionId + '"]').addClass('active');
+			}
+
+			if (scrollTop < introH / 2) {
+				$('[data-scroll]').removeClass('active');
+			}
+		});
+	}
 });

5e53e10630d11c13595bd514423c5d79ec455a7a
neginfinity85 <neginfinity85@gmail.com>
2023-08-10 21:26:38 +0200

    Added <jQuery function> to <Show-Hide> modal window <Become a client>

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,4 +76,53 @@ $(function () {
 			}
 		});
 	}
+
+	/* Modal windows show-hide
+	=========================================== */
+
+	$('[data-modal]').on('click', function (event) {
+		event.preventDefault();
+
+		let modal = $(this).data('modal');
+
+		$('body').addClass('no-scroll');
+		$(modal).addClass('show');
+
+		setTimeout(function () {
+			$(modal).find('.modal__content').css({
+				transform: 'scale(1)',
+				opacity: '1',
+			});
+		}, 200);
+	});
+
+	$('[data-modal-close]').on('click', function (event) {
+		event.preventDefault();
+
+		let modal = $(this).parents('.modal');
+
+		modalClose(modal);
+	});
+
+	$('.modal').on('click', function () {
+		let modal = $(this);
+
+		modalClose(modal);
+	});
+
+	$('.modal__content').on('click', function (event) {
+		event.stopPropagation();
+	});
+
+	function modalClose(modal) {
+		modal.find('.modal__content').css({
+			transform: 'scale(0.5)',
+			opacity: '0',
+		});
+
+		setTimeout(function () {
+			$('body').removeClass('no-scroll');
+			modal.removeClass('show');
+		}, 200);
+	}
 });
